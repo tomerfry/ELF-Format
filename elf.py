8fc0adce822c7e2d365ba75f39e2d06895444578
tomerfry <sabrina@DESKTOP-T52SCE5.localdomain>
2020-04-09 19:57:27 +0300

    Added support for shdrs parsing.

--- elf.py
+++ elf.py
@@ -20,9 +20,13 @@ class Elf(object):
 
         self.phdrs = parse_phdrs(self.ehdr, self.file_content)
 
+        self.shdrs = parse_shdrs(self.ehdr, self.file_content)
+
+
     def save_as(self, file_name):
         raw_elf_header = pack_elf_header(self.ehdr)
         raw_phdrs = pack_phdrs(self.phdrs)
+        raw_shdrs = pack_shdrs(self.shdrs)
 
         with open(file_name, 'wb') as new_file:
             new_file.write(self.file_content)
@@ -30,3 +34,5 @@ class Elf(object):
             new_file.write(raw_elf_header)
             new_file.seek(self.ehdr.get('e_phoff'))
             new_file.write(raw_phdrs)
+            new_file.seek(self.ehdr.get('e_shoff'))
+            new_file.write(raw_shdrs)
--- elf_utils.py
+++ elf_utils.py
@@ -13,17 +13,23 @@ ELF_MAGIC = b'\x7fELF'
 ELF_MAGIC_LEN = 4
 
 CLASS_ARCH_64 = 2
-DATA_2_LSB = 1
 
 PHDR_FORMAT = '<2I6Q'
 PHDR_ENTRY_FIELDS = ['p_type', 'p_flags', 'p_offset', 'p_vaddr',
                      'p_paddr', 'p_filesz', 'p_memsz', 'p_align']
+
+SHDR_INVALID_OFFSET = 0
+SHDR_FORMAT = '<2I4Q2I2Q'
+SHDR_ENTRY_FIELDS = ['sh_name', 'sh_type', 'sh_flags', 'sh_addr', 'sh_offset',
+					 'sh_size', 'sh_link', 'sh_info', 
+					 'sh_addralign', 'sh_entsize']
 
 
 def parse_elf_header(raw_ehdr):
     ehdr_values = struct.unpack(ELF_HEADER_FORMAT, raw_ehdr)
     ehdr = collect_struct_fields(ELF_HEADER_FIELDS, ehdr_values)
     return ehdr
+
 
 def parse_phdrs(ehdr, file_content):
     phoff = ehdr.get('e_phoff')
@@ -34,16 +40,41 @@ def parse_phdrs(ehdr, file_content):
     phdrs = []
 
     for offset in range(0, len(raw_phdrs), phentsize):
-        phdr = raw_phdrs[offset: offset + phentsize]
-        phdrs.append(parse_phdr(phdr))
+        raw_phdr = raw_phdrs[offset: offset + phentsize]
+        phdrs.append(parse_phdr(raw_phdr))
     return phdrs
+
 
 def parse_phdr(raw_phdr):
     phdr_values = struct.unpack(PHDR_FORMAT, raw_phdr)
     return collect_struct_fields(PHDR_ENTRY_FIELDS, phdr_values)
 
+
+def parse_shdrs(ehdr, file_content):
+	shoff = ehdr.get('e_shoff')
+	shnum = ehdr.get('e_shnum')
+	shentsize = ehdr.get('e_shentsize')
+
+	if shoff == SHDR_INVALID_OFFSET:
+		return None
+
+	raw_shdrs = file_content[shoff:shoff+(shnum * shentsize)]
+	shdrs = []
+
+	for offset in range(0, len(raw_shdrs), shentsize):
+		raw_shdr = raw_shdrs[offset:offset+shentsize]
+		shdrs.append(parse_shdr(raw_shdr))
+	return shdrs
+
+
+def parse_shdr(raw_shdr):
+	shdr_values = struct.unpack(SHDR_FORMAT, raw_shdr)
+	return collect_struct_fields(SHDR_ENTRY_FIELDS, shdr_values)
+
+
 def pack_elf_header(ehdr):
     return struct.pack(ELF_HEADER_FORMAT, *ehdr.values())
+
 
 def pack_phdrs(phdrs):
     raw_phdrs = b''
@@ -51,6 +82,15 @@ def pack_phdrs(phdrs):
     for phdr in phdrs:
         raw_phdrs += struct.pack(PHDR_FORMAT, *phdr.values())
     return raw_phdrs
+
+
+def pack_shdrs(shdrs):
+	raw_shdrs = b''
+
+	for shdr in shdrs:
+		raw_shdrs += struct.pack(SHDR_FORMAT, *shdr.values())
+	return raw_shdrs
+
 
 def collect_struct_fields(field_names, values):
     od = OrderedDict()
